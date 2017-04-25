669a190f9fa9463db06818655bf1ce77ba52b19e
Jared Still <jkstill@gmail.com>
2017-04-24 18:14:07 -0700

    Appears to be correct...

--- fixdup.pl
+++ fixdup.pl
@@ -6,6 +6,8 @@ use warnings;
 use Data::Dumper;
 use Digest::MD5;
 
+my $debug=0;
+
 my %addresses=();
 
 =head1 Data Format
@@ -49,9 +51,9 @@ my %addressHash=();
 
 while (<>) {
 
-	chomp;
-	# remove leading/trailing spaces
-	#s/\s+,\s+/,/g;
+	#chomp; # do not use if unix2dos has been run on the input files
+	# remove leading space in OR (state)
+	s/ OR/OR/g;
 
 	my @data = split(/,/);
 	my $md5 =  Digest::MD5->new;
@@ -76,19 +78,21 @@ while (<>) {
 	}
 
 	# get names and phones
-	push @{$addresses{$data[STREETADDRESS]}->{'NAMES'}{$data[LNAME]}}, [ $data[FNAME], $data[PHONE]];
 
 	# the following fields will be useful for sorting
-	$addresses{$data[STREETADDRESS]}->{'CITY'} = $data[CITY];
-	$addresses{$data[STREETADDRESS]}->{'STATE'} = $data[STATE];
-	$addresses{$data[STREETADDRESS]}->{'ZIPCODE'} = $data[ZIPCODE];
-	$addresses{$data[STREETADDRESS]}->{'STREETNUM'} = $data[STREETNUM];
-	$addresses{$data[STREETADDRESS]}->{'STREETNAME'} = qq{$data[STREETLOC] $data[STREETNAME] $data[STREETDESG]};
+	my $key = $data[STREETADDRESS] . '-' . $data[CITY];
+
+	push @{$addresses{$key}->{'NAMES'}{$data[LNAME]}}, [ $data[FNAME], $data[PHONE]];
+	$addresses{$key}->{'CITY'} = $data[CITY];
+	$addresses{$key}->{'STATE'} = $data[STATE];
+	$addresses{$key}->{'ZIPCODE'} = $data[ZIPCODE];
+	$addresses{$key}->{'STREETNUM'} = $data[STREETNUM];
+	$addresses{$key}->{'STREETNAME'} = qq{$data[STREETLOC] $data[STREETNAME] $data[STREETDESG]};
 
 }
 
 
-print 'Addresses: ' , Dumper(\%addresses);
+print 'Addresses: ' , Dumper(\%addresses) if $debug;
 
 warn "$noAddress records skipped due to no address\n";
 warn "$dupAddress records skipped as duplicates\n";
@@ -113,11 +117,11 @@ my $cleanData ;
 my $delimiter=',';
 
 foreach my $address (keys %addresses) {
-	print "### Working on $address\n";
+	print "### Working on $address\n" if $debug;
 
 	# make a copy of the current record to simplify code
 	my $data = $addresses{$address};
-	print 'Work Data: ' , Dumper($data);
+	print 'Work Data: ' , Dumper($data) if $debug;
 
 
 	$cleanData->{$address}{'STREETNAME'} = $data->{'STREETNAME'};
@@ -128,40 +132,47 @@ foreach my $address (keys %addresses) {
 
 	# get count of different phone numbers
 	my @lastNames = keys %{$data->{'NAMES'}};
-	print '@lastNames: ' , Dumper(\@lastNames), "\n";
+	print '@lastNames: ' , Dumper(\@lastNames), "\n" if $debug;
 
 	foreach my $lastName ( @lastNames ) {
-	my %phoneHash;
 
-		$cleanData->{$address}{'LASTNAME'} = $lastName;
+		my %phoneHash;
+
+		#$cleanData->{$address}{'LASTNAME'} = $lastName;
 
 		my $firstNames = $data->{'NAMES'}{$lastName};
-		print "== LastName: $lastName\n";
-		print "FirstNames: " , Dumper($firstNames);
+		print "== LastName: $lastName\n" if $debug;
+		print "FirstNames: " , Dumper($firstNames) if $debug;
 
 		foreach my $el ( 0..$#{$firstNames} ) {
-			#print "phonetest: $firstNames->[$el][1]\n";
-			#print "fname: $firstNames->[$el][0]\n";
+			#print "phonetest: $firstNames->[$el][1]\n" if $debug;
+			#print "fname: $firstNames->[$el][0]\n" if $debug;
 			$phoneHash{$firstNames->[$el][1]} = 'Phone';  # dummy value	
 		}
 
-		#print 'Dumper %phoneHash: ' || Dumper(\%phoneHash), "\n";
-		#print 'keys %phoneHash: ' , join (' - ', keys %phoneHash), "\n";
+		#print 'Dumper %phoneHash: ' , Dumper(\%phoneHash), "\n" if $debug;
+		#print 'keys %phoneHash: ' , join (' - ', keys %phoneHash), "\n" if $debug;
 
 		my @phones = keys %phoneHash;
-		print 'Phone Data: ' , join (' | ' , @phones), "\n";
+		print 'Phone Data: ' , join (' | ' , @phones), "\n" if $debug;
 
 		my $phoneCount = $#phones;
 
-		print "Last Names: ", join(' | ', @lastNames), "\n";
+		print "Last Names: ", join(' | ', @lastNames), "\n" if $debug;
+
+
+		foreach my $el ( 0 ..$#{$firstNames}) {
+			push @{$cleanData->{$address}{'NAMES'}{$lastName}}, $firstNames->[$el][0];
+		}
 
 		if ($phoneCount < 0 ) {
 			$cleanData->{$address}{'PHONE'}{$lastName} = 'NA';
 		} elsif ($phoneCount > 0 ) {
 				foreach my $el ( 0 ..$#{$firstNames}) {
-					print "LastName: $lastName\n";
+					print "LastName: $lastName\n" if $debug;
 					$cleanData->{$address}{'PHONE'}{$lastName} .= qq{$firstNames->[$el][0] ->  $firstNames->[$el][1]  | };
 				}
+				$cleanData->{$address}{'PHONE'}{$lastName} = substr($cleanData->{$address}{'PHONE'}{$lastName},0,length($cleanData->{$address}{'PHONE'}{$lastName})-2);
 		} else {
 			$cleanData->{$address}{'PHONE'}{$lastName} .= $phones[0];
 		}
@@ -170,8 +181,30 @@ foreach my $address (keys %addresses) {
 
 }
 
-print 'CleanData: ' . Dumper($cleanData);
+print 'CleanData: ' . Dumper($cleanData) if $debug;
 
+print "StreetNum,StreetName,City,State,ZipCode,LastName,FirstNames,Phones\n";
+
+foreach my $address ( keys %{$cleanData} ) {
+
+	# are there multiple last names?
+	# print 2 records if so
+	# 
+	my @lastNameAry = (keys %{$cleanData->{$address}{'NAMES'}});
+	foreach my $lastName ( @lastNameAry ) {
+		print "$cleanData->{$address}{'STREETNUM'},";
+		print "$cleanData->{$address}{'STREETNAME'},";
+		print "$cleanData->{$address}{'CITY'},";
+		print "$cleanData->{$address}{'STATE'},";
+		print "$cleanData->{$address}{'ZIPCODE'},";
+		print "$lastName,";
+		print join(' ',@{$cleanData->{$address}{'NAMES'}{$lastName}}),',';
+		print $cleanData->{$address}{'PHONE'}{$lastName};
+
+		print "\n";
+
+	}
+}
 
 
 
